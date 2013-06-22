7fd2494ea0e8146ce7db8841e65f1f6a4cbd9703
Greg MacGregor <gmacgregor@gmail.com>
2013-06-22 18:25:19 -0400

    fixed issue with search method

--- parsely.js
+++ parsely.js
@@ -18,6 +18,34 @@ parsely.prototype.get = function (path, options, callback) {
     this.request('GET', path, options, callback);
 };
 
+parsely.prototype.postDetail = function (url, days, callback) {
+    if (typeof days === 'function') {
+        callback = days;
+        days = null;
+    }
+    var self = this,
+        opts = {
+            'url': url,
+            'days': days
+        };
+    this.get('analytics/post/detail', opts, function (body) {
+        var detail = JSON.parse(body).data[0];
+        self.postShares(url, function (shareBody) {
+            var shares = JSON.parse(shareBody).data[0];
+            detail.shares = shares;
+            callback(detail);
+        });
+    });
+};
+
+parsely.prototype.postShares = function (url, callback) {
+    this.get('shares/post/detail', {'url': url}, callback);
+};
+
+parsely.prototype.realtime = function (type, options, callback) {
+    this.get('realtime/' + type, options, callback);
+};
+
 parsely.prototype.request = function (method, path, options, callback) {
     if (method !== 'GET') {
         throw new Error('Unsupported method: ' + method + '\n');
@@ -41,21 +69,12 @@ parsely.prototype.request = function (method, path, options, callback) {
     });
 };
 
-parsely.prototype.postDetail = function (url, days, callback) {
-    if (typeof days === 'function') {
-        callback = days;
-        days = null;
+parsely.prototype.search = function (query, options, callback) {
+    if (typeof options === 'function') {
+        callback = options;
+        options = null;
     }
-    var opts = {
-        'url': url,
-        'days': days
-    };
-    this.get('analytics/post/detail', opts, callback);
-};
-
-
-parsely.prototype.search = function (query, callback) {
-    this.get('search', {'q': query}, callback);
+    this.get('search', _.extend({'q': query}, options), callback);
 };
 
 module.exports = new parsely();
