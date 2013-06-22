8802fc7ff91b9f5b42d432f41615006150805cfd
Greg MacGregor <gmacgregor@gmail.com>
2013-06-22 18:25:56 -0400

    added tests for search helper

--- test/test.js
+++ test/test.js
@@ -4,24 +4,20 @@ var should = require('should');
 
 describe('parsely', function () {
 
-    describe('configuration', function () {
+    it('should have an endpoint', function () {
+        parsely.ROOT.should.equal('http://api.parsely.com');
+    });
 
-        it('should have an endpoint', function () {
-            parsely.ROOT.should.equal('http://api.parsely.com');
-        });
+    it('should be version v2', function () {
+        parsely.VERSION.should.equal('v2');
+    });
 
-        it('should be version v2', function () {
-            parsely.VERSION.should.equal('v2');
-        });
+    it('should have an api key', function () {
+        parsely.KEYS.apikey.should.not.equal('');
+    });
 
-        it('should have an api key', function () {
-            parsely.KEYS.apikey.should.not.equal('');
-        });
-
-        it('should have a shared secret', function () {
-            parsely.KEYS.secret.should.not.equal('');
-        });
-
+    it('should have a shared secret', function () {
+        parsely.KEYS.secret.should.not.equal('');
     });
 
     describe('response', function () {
@@ -34,6 +30,25 @@ describe('parsely', function () {
             });
         });
 
+
+    });
+
+    describe('search helper', function () {
+
+        it('can accept options', function (done) {
+            var opts = {page: 1, limit: 1};
+            parsely.search('Canada', opts, function (response) {
+                JSON.parse(response).should.ownProperty('data');
+            });
+            done();
+        });
+
+        it('options should be optional', function (done) {
+            parsely.search('Canada', function (response) {
+                JSON.parse(response).should.ownProperty('data');
+            });
+            done();
+        });
     });
 
 });
